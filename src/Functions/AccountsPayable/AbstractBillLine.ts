df336e9c679df00aa34be6083eb5ce531c8b263e
OG <orlando.geronimo@gmail.com>
2022-05-07 22:33:46 -0400

    Update AbstractBillLine.ts

--- src/Functions/AccountsPayable/AbstractBillLine.ts
+++ src/Functions/AccountsPayable/AbstractBillLine.ts
@@ -12,7 +12,7 @@
  * http://www.apache.org/licenses/LICENSE-2.0
  *
  * or in the "LICENSE" file accompanying this file. This file is distributed on
- * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either 
  * express or implied. See the License for the specific language governing
  * permissions and limitations under the License.
  */
